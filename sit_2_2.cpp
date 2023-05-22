5fd0308e4ecadde86a79c9cef57e9a105da47f66
Дмитрий Живодеров <zhivoderovd@icloud.com>
2023-05-22 11:28:34 +0400

    // 2 номер (фибоначи)

---
+++ sit_2_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+
+int fib(int n) {
+
+	if (n < 1) {
+		return 0;
+	}
+	if (n == 1) {
+		return 1;
+	}
+	else { // рекурсивный случай
+		return fib(n - 1) + fib(n - 2);
+	}
+}
+
+int main() {
+	int n; // вычисляем 10-ое число Фибоначчи
+	std::cout << "Enter n" << std::endl;
+	std::cin >> n;
+	int result = fib(n);
+	std::cout << "Fibonacci(" << n << ") = " << result << std::endl;
+	return 0;
+}
