7db995997a102739de0ad100212fdd661609cec3
Дмитрий Живодеров <zhivoderovd@icloud.com>
2023-05-22 11:42:59 +0400

    // 3 номер (фибоначи)

---
+++ sit_2_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int fib(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	if (n == 1) {
+		return 1;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+	int n;
+	std::cout << "Enter a number n: ";
+	std::cin >> n;
+
+	std::cout << "First " << n << " Fibonacci numbers:" << std::endl;
+
+	for (int i = 0; i < n; i++) {
+		std::cout << fib(i) << std::endl;
+	}
+
+	return 0;
+}
